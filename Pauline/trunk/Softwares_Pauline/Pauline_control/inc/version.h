12b2166a14fce66a4ac0c69422d5736d70a5e577
mIHA <miha@sowalabs.com>
2021-06-06 14:31:10 +0200

    Initial commit

---
+++ Pauline/trunk/Softwares_Pauline/Pauline_control/inc/version.h
@@ -0,0 +1,5 @@
+#define FILE_VERSION        1,2,3,1
+#define STR_FILE_VERSION   "1,2,3,1\0"
+#define STR_FILE_VERSION2  "1.2.3.1"
+#define STR_DATE           "18 January 2020"
+
---
+++ libhxcfe/trunk/sources/version.h
@@ -0,0 +1,10 @@
+#define FILE_VERSION        2,13,6,12
+#define STR_FILE_VERSION   "2,13,6,12\0"
+#define STR_FILE_VERSION2  "2.13.6.12"
+#define STR_DATE           "19 May 2021\0"
+#define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
+
+#define FILEVER        FILE_VERSION
+#define PRODUCTVER     FILE_VERSION
+#define STRFILEVER     STR_FILE_VERSION
+#define STRPRODUCTVER  STR_FILE_VERSION
